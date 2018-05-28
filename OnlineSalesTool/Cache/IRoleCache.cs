e2d6a9815f98da454e43defaea1730c5a9a119a9
Luu Hong <donkeizluv@gmail.com>
2018-05-28 20:40:44 +0700

    cooler search bar

---
+++ OnlineSalesTool/Cache/IRoleCache.cs
@@ -0,0 +1,13 @@
+﻿using OnlineSalesTool.AppEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineSalesTool.Cache
+{
+    public interface IRoleCache
+    {
+        void GetRoleId(string role, out int roleId, out RoleEnum appRole);
+    }
+}
---
+++ OnlineSalesTool/Cache/RoleCache.cs
@@ -0,0 +1,34 @@
+﻿using OnlineSalesTool.AppEnum;
+using OnlineSalesTool.CustomException;
+using OnlineSalesTool.EFModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineSalesTool.Cache
+{
+    public class RoleCache : IRoleCache
+    {
+        private static IDictionary<string, int> _roleDict;
+
+        public RoleCache(OnlineSalesContext context)
+        {
+            if (context == null) throw new ArgumentNullException();
+            if (_roleDict != null) return;
+            _roleDict = context.UserRole.Select(u => new { u.RoleId, u.Name }).
+                ToDictionary(key => key.Name, id => id.RoleId);
+        }
+
+        public void GetRoleId(string role, out int roleId, out RoleEnum appRole)
+        {
+            if (string.IsNullOrEmpty(role))
+                throw new ArgumentNullException();
+            if (!_roleDict.ContainsKey(role))
+                throw new BussinessException($"No such role found in database: {role}");
+            if(!Enum.TryParse(role, true, out appRole))
+                throw new BussinessException($"Invalid role: {role}");
+            roleId = _roleDict[role];
+        }
+    }
+}
